5f666aad7c7ed152e3bb51759b051613155e024b
Akshay269 <aksrahangdale@gmail.com>
2025-09-03 23:55:03 +0530

    backend build

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./src/routes/authRoutes');
-const jobRoutes=require('./src/routes/jobRoutes');
-const appRoutes=require('./src/routes/appRoutes');
-const cloudRoutes=require('./src/routes/cloudRoutes');
-const draftRoutes=require('./src/routes/draftRoutes');
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./src/routes/authRoutes");
+const jobRoutes = require("./src/routes/jobRoutes");
+const appRoutes = require("./src/routes/appRoutes");
+const cloudRoutes = require("./src/routes/cloudRoutes");
+const draftRoutes = require("./src/routes/draftRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 // await initGoogleAuth();
-app.use('/api/auth', authRoutes);
-app.use('/api/jobs',jobRoutes);
-app.use('/api/applications',appRoutes);
-app.use('/api/cloudinary',cloudRoutes);
-app.use('/api/drafts',draftRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/jobs", jobRoutes);
+app.use("/api/applications", appRoutes);
+app.use("/api/cloudinary", cloudRoutes);
+app.use("/api/drafts", draftRoutes);
 
 module.exports = app;
